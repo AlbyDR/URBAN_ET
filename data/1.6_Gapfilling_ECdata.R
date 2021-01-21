8a6d88dd055e9b1be7603d9fc0837f149809158c
Alby Duarte Rocha <40297927+AlbyDR@users.noreply.github.com>
2021-01-21 13:15:41 +0100

    Update and rename Gapfill.R to 1.6_Gapfilling_ECdata.R

--- data/1.6_Gapfilling_ECdata.R
+++ data/1.6_Gapfilling_ECdata.R
@@ -1,12 +1,12 @@
-# see REddyProc typical workflow (help)
-# REddyProc::useCase
 ########################################################################
-library(REddyProc)  # gap-filling
-library(lubridate)
-library(ggplot2)
-library(dplyr)
-library(MeTo)
+library(REddyProc)  # gap-filling # see REddyProc typical workflow (help) or REddyProc::useCase
+library(lubridate)  # year
+library(dplyr)      # filter
+library(MeTo)       # satVP
+library(bigleaf)    # LE.to.ET
+library(zoo)        # na.aprox
 ############################################################################
+# get a database to copy the structure
 ############################################################################
 #+++ Load data with 1 header and 1 unit row from (tab-delimited) text file
 fileName <- getExamplePath('Example_DETha98.txt', isTryDownload = TRUE)
@@ -21,31 +21,33 @@ EddyDataWithPosix <- fConvertTimeToPosix(
   filterLongRuns("NEE")
 ############################################################################
 ############################################################################
+# create a data.frame with the required variables as the example above
 Gap_ROTH <- data.frame(DataTime=EC_ROTH$timestamp,
                        Year=EC_ROTH$year,
                        DoY=EC_ROTH$DOY,
                        Hour=EC_ROTH$hour,
                        NEE=EC_ROTH$CO2_sig_strgth_mean,
-                       LE=EC_ROTH$LE.dry6.fsd,
+                       LE=EC_ROTH$LE.dry6.fsd, # cleaned LE
                        H=EC_ROTH$H,
                        Rg=EC_ROTH$Rin,
                        Tair=EC_ROTH$Ta,
                        Tsoil=EC_ROTH$Ta-5,
                        rH=EC_ROTH$RH,
                        VPD=satVP(Tmean=EC_ROTH$Ta,interval='hour')*10*(1-EC_ROTH$RH/100),
-                       Ustar=sdClean(EC_ROTH$u.,3))
+                       Ustar=sdClean(EC_ROTH$u.,3)) # excluding extreme values
 
 colnames(Gap_ROTH) <- colnames(EddyDataWithPosix) 
 
-#Gap_ROTH$season <- month(EC_ROTH$timestamp)
-Gap_ROTH$L=sdClean(EC_ROTH$L,3)
+#including data require to footprint 
+Gap_ROTH$L=sdClean(EC_ROTH$L,3) # excluding extreme values
 Gap_ROTH$wd=EC_ROTH$wd
-Gap_ROTH$ws=sdClean(EC_ROTH$ws,5)
-Gap_ROTH$v_var=sdClean(EC_ROTH$v_var,3)
+Gap_ROTH$ws=sdClean(EC_ROTH$ws,5) # excluding extreme values
+Gap_ROTH$v_var=sdClean(EC_ROTH$v_var,3) # excluding extreme values
 
-G_Roth <- sEddyProc$new('Roth', Gap_ROTH, c('NEE','LE','Rg','Tair','VPD','Ustar',
+# Variables for gapfilling
+G_Roth <- sEddyProc$new('ROTH', Gap_ROTH, c('NEE','LE','Rg','Tair','VPD','Ustar',
                                              'v_var','ws','wd','L','rH'))
-                                             
+# set location                                             
 G_Roth$sSetLocationInfo(LatDeg = 52.45, LongDeg = 13.32, TimeZoneHour = 0)  #Location of Gebesee
 
 #++ Fill NEE gaps with MDS gap filling algorithm (without prior ustar filtering)
@@ -63,18 +65,10 @@ FilledLE_Roth <- G_Roth$sExportResults()
 G_Roth$sPlotFingerprintY('LE_orig', Year = 2019)
 G_Roth$sPlotFingerprintY('LE_f', Year = 2019)
 
+# including date variables
 FilledLE_Roth$timestamp <- EC_ROTH$timestamp
-FilledLE_Roth$year <- year(EC_ROTH$timestamp)
-FilledLE_Roth$month <- month(EC_ROTH$timestamp)
-FilledLE_Roth$week <- week(EC_ROTH$timestamp)
-FilledLE_Roth$DoY <- yday(EC_ROTH$timestamp)
-FilledLE_Roth$hour <- hour(EC_ROTH$timestamp)
-
 FilledLE_Roth <- data.frame(FilledLE_Roth)
-
-summary(filter(FilledLE_Roth, year==2019)$LE_f)
-sum(filter(FilledLE_Roth, year==2019 & LE_f>=0)$LE_f)
-
+# including filled variables to EC_data
 EC_ROTH$LE.filled <- FilledLE_Roth$LE_f
 EC_ROTH$u.filled <- FilledLE_Roth$Ustar_f
 EC_ROTH$L.filled <- FilledLE_Roth$L_f
@@ -84,12 +78,7 @@ EC_ROTH$v_var.filled <- FilledLE_Roth$v_var_f
 
 summary(EC_ROTH)
 
-summary(EC_ROTH)
-summary(EC_ROTH$LE.filled)
-summary(FilledLE_Roth$LE_f)
-
-library(zoo)
-library(bigleaf)
+# convert LE to ET
 ET_ROTH.Filled <- na.approx(LE.to.ET(filter(EC_ROTH,year(timestamp)==2019)$LE.filled,
                                      filter(EC_ROTH,year(timestamp)==2019)$Ta)*3600)
 
@@ -97,10 +86,5 @@ ET_ROTH.Filled[which(ET_ROTH.Filled<=0)] <- 0
 sum(ET_ROTH.Filled)
 ###################################################################
 write.csv(EC_ROTH,
-          file="C:/Users/Alby Rocha/Documents/EC/FPmaps/data/EC_ROTH.csv", 
+          #file="C:/Users/Alby Rocha/Documents/EC/FPmaps/data/EC_ROTH.csv", 
           row.names = F)
-
-#EC_ROTH19 <- filter(EC_ROTH, year(timestamp)==2019)
-#write.csv(EC_ROTH19,
-#          file="C:/Users/Alby Rocha/Documents/EC/FPmaps/data/EC_ROTH19.csv", 
-#          row.names = F)
